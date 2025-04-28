2927b1a003d30c20f9aa9825e4c51bb3c1966b64
LeoBaiz <leonardobaiz@hotamail.com>
2025-04-28 20:19:59 -0300

    Actualización de Proyecto

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,3 @@
-BASE_URL_Stable = "http://testing-stable.expand/expand"
-BASE_URL_Unstable = "http://testing-unstable.expand/expand"
+BASE_URL = "https://the-internet.herokuapp.com"
 BROWSER = "chrome"
 TIMEOUT = 2
